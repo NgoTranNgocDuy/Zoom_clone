85780e19f3bed3a0e696b8cd0cf916bfc47909d1
NgocDuy1234 <tngo0031@student.monash.edu>
2024-11-10 17:27:21 +1100

    Add middleware and authentication for the app

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,9 +3,7 @@ import React from 'react'
 const RootLayout = ({children}: {children: React.ReactNode}) => {
   return (
     <main>
-        Navbar
         {children}
-        Footer
     </main>
   )
 }
