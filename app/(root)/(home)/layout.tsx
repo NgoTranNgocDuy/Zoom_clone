ac8cca4aebc2ea31aad46ba32e72a3e3ad2e61c5
NgocDuy1234 <tngo0031@student.monash.edu>
2024-11-10 15:41:48 +1100

    Add the asset folder

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,14 +1,28 @@
-import React from 'react'
-// This will do some specific things to every single page
-const HomeLayout
- = ({children}: {children: React.ReactNode}) => {
-  return (
-    <main className='relative'>
-        Navbar
-        {children}
-        Footer
-    </main>
-  )
-}
+import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
+import { Metadata } from 'next';
+import { ReactNode } from 'react';
 
-export default HomeLayout
+
+export const metadata: Metadata = {
+  title: 'YOOM',
+  description: 'A workspace for your team, powered by Stream Chat and Clerk.',
+};
+
+const RootLayout = ({ children }: Readonly<{children: ReactNode}>) => {
+  return (
+    <main className="relative">
+      <Navbar />
+
+      <div className="flex">
+        <Sidebar />
+        
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+          <div className="w-full">{children}</div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default RootLayout;
